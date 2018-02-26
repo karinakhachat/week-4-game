f8a965313fb6c4f34f4449bb30099f9d245cf70b
Karina Khachatryan <karinakhachat@Karinas-Air.fios-router.home>
2018-02-26 10:34:18 -0500

    updated formatting

--- assets/javascript/Javascript.js
+++ assets/javascript/Javascript.js
@@ -1,60 +1,62 @@
 
-  $(document).ready(function() 
-  //the largest parent function start 
-  {
-    var randomnum = Math.floor(Math.random()*102+19);
+$(document).ready(function ()
+//the largest parent function start 
+{
+    var randomnum = Math.floor(Math.random() * 102 + 19);
     var usertotal = 0;
     var wins = 0;
     var losses = 0;
-//logging values into the html elements
-
-    $("#numberToGuess").text (randomnum);
+    //logging values into the html elements
+    //working thus far
+    $("#numberToGuess").text(randomnum);
     $("#wins").text(wins);
     $("#losses").text(losses);
+    //this part is working as expected
+    function init() {
+        var CrysVals = {};
+        imageCrystal1[1] = Math.floor(Math.random() * 12 + 1);
+        imageCrystal2[2] = Math.floor(Math.random() * 12 + 1);
+        imageCrystal3[3] = Math.floor(Math.random() * 12 + 1);
+        imageCrystal4[4] = Math.floor(Math.random() * 12 + 1);
 
-    function init (){
-    var CrysVals ={};
-    imageCrystal1[1] = Math.floor(Math.random()*12+1);
-    imageCrystal2[2] = Math.floor(Math.random()*12+1);
-    imageCrystal3[3] = Math.floor(Math.random()*12+1);
-    imageCrystal4[4]= Math.floor(Math.random()*12+1);
+        $("#image1").on("click", CrysVals(1));
+        $("#image2").on("click", CrysVals(2));
+        $("#image3").on("click", CrystalVals(3));
+        $("#image4").on("click", CrystalVals(4));
+        //not working not assigning images crystal values
 
-    $("#image1").on("click", CrysVals(1));
-    $("#image2").on("click", CrysVals(2));
-    $("#image3").on("click", CrystalVals(3));
-    $("#image4").on("click", CrystalVals(4));
+        function NewTotal(crystalKey) {
+            usertotal = usertotal + CrysVals[NewTotal];
+            console.log("new total" + usertotal);
+            $("#YourTotal").text(usertotal);
 
-    function NewTotal(crystalKey) {
-        usertotal = usertotal + CrysVals[NewTotal];
-        console.log ("new total" + usertotal);
-        $("#YourTotal").text(usertotal);
+            if (usertotal === randomnum) 
+            {
+                wins++;
+                $("#wins").text(wins);
+            }
 
-        if (usertotal === randomnum) {
-            wins++;
-            $("#wins").text(wins);
-        }
-
-        else if ( usertotal  > randomnum)
-        losses++;
-        $("#losses").text(losses);
-    } 
-}
-    function reset() 
-    {
+            else if (usertotal > randomnum)
+            {
+                losses++;
+            $("#losses").text(losses);
+            }
+    }
+    function reset() {
         if (wins++)
-        usertotal.clear();
-        wins.clear();
-        losses.clear();
+            usertotal.clear();
+            wins.clear();
+            losses.clear();
 
         if (losses++)
 
-        usertotal.clear();
-        wins.clear();
-        losses.clear();
-         
+            usertotal.clear();
+            wins.clear();
+            losses.clear();
+
     }
 
-init();
-reset();
+    init();
+    reset();
 
 })
