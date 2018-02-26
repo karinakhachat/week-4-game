2dcbd17a3197cf06bc2dc56154853d2a577964a1
Karina Khachatryan <karinakhachat@Karinas-Air.fios-router.home>
2018-02-25 22:36:52 -0500

    cleaner and more efficient

--- assets/javascript/Javascript.js
+++ assets/javascript/Javascript.js
@@ -2,50 +2,16 @@
   $(document).ready(function() 
   //the largest parent function start 
   {
-    var CrysVals ={};
-    imageCrystal1[1] = Math.floor(Math.random()*12+1);
-    imageCrystal2[2] = Math.floor(Math.random()*12+1);
-    imageCrystal3[3] = Math.floor(Math.random()*12+1);
-    imageCrystal4[4]= Math.floor(Math.random()*12+1);
-//appends crystal attributes
-    {
-      var imageCrystal1 = $('<img>');
-      
-      imageCrystal1.attr('data-num', number1[i]);
-      imageCrystal1.attr('src', 'assets/images/greencrystal.png');
-      imageCrystal1.attr('alt', 'green crystal');
-      imageCrystal1.addClass('crystalImage');
-      $('#crystals').append(imageCrystal1);
-    }
-    
-    {
-      var imageCrystal2 = $('<img>');
-      
-      imageCrystal2.attr('data-num', number2[i]);
-      imageCrystal2.attr('src', 'assets/images/bluecrystal.png');
-      imageCrystal2.attr('alt', 'blue crystal');
-      imageCrystal2.addClass('crystalImage');
-      $('#crystals').append(imageCrystal2);
-    }
-    {
-      var imageCrystal3 = $('<img>');
-      
-      imageCrystal3.attr('data-num', number3[i]);
-      imageCrystal3.attr('src', 'assets/images/redcrystal.png');
-      imageCrystal3.attr('alt', 'red crystal');
-      imageCrystal3.addClass('crystalImage');
-      $('#crystals').append(imageCrystal3);
-    }
-    {
-      var imageCrystal4 = $('<img>');
-      
-      imageCrystal4.attr('data-num', number4[i]);
-      imageCrystal4.attr('src', 'assets/images/purplecrystal.png');
-      imageCrystal4.attr('alt', 'purple crystal');
-      imageCrystal4.addClass('crystalImage');
-      $('#crystals').append(imageCrystal4);
-      
-    }
+    var randomnum = Math.floor(Math.random()*102+19);
+    var usertotal = 0;
+    var wins = 0;
+    var losses = 0;
+//logging values into the html elements
+
+    $("#numbertoGuess").text (randomnum);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+
 
     var CrysVals ={};
     imageCrystal1[1] = Math.floor(Math.random()*12+1);
@@ -53,5 +19,25 @@
     imageCrystal3[3] = Math.floor(Math.random()*12+1);
     imageCrystal4[4]= Math.floor(Math.random()*12+1);
 
-  function 
-  })
+
+    function CrystalValue (crystalKey) {
+        usertotal = usertotal + CrysVals[crystalKey];
+        console.log ("new total" + usertotal);
+        $("#YourTotal").text(usertotal);
+
+        if (usertotal === randomnum) {
+            wins++;
+            $("#wins").text(wins);
+        }
+
+        else if ( usertotal  > randomnum)
+        losses++;
+        $("#losses").text(losses);
+    }
+ 
+    $("#image1").on("click", CrystalValue(1));
+    $("#image2").on("click", CrystalValue(2));
+    $("image3").on("click", CrystalValue(3));
+    $("image4").on("click", CrystalValue(4));
+
+})
