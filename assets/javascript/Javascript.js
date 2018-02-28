f98e21f0b1a7b192b0bf280cfe036e6fb6d6ebd4
Karina Khachatryan <karinakhachat@Karinas-MacBook-Air.local>
2018-02-28 10:00:51 -0500

    update 2

--- assets/javascript/Javascript.js
+++ assets/javascript/Javascript.js
@@ -1,49 +1,67 @@
-
-$(document).ready(function ()
-//the largest parent function start 
-{})
-    
-    var usertotal = 0;
-    var wins = 0;
-    var losses = 0
-    var targetnumber = Math.floor(Math.random()* 120) -1;
-  
-    //logging values into the html elements
-    //working thus far
-    $("#numberToGuess").text(targetnumber);
-    $("#wins").text(wins);
-    $("#losses").text(losses);
-    //this part is working as expected
-    
-    function init(){
-          function start(){
-            var images = [];
-            $(".crystals").each(function(){
-              images.push($(this).attr('src'))
-            })
-            console.log(images);
-          }
-           
-        }
-      console.log(randomnum);
-  
+$(document).ready(function (){
+    //the largest parent function start 
+    {
+        
+        var usertotal = 0;
+        var wins = 0;
+        var losses = 0
+        var targetnumber = Math.floor(Math.random()* 120) -1;
       
-        //not working not assigning images crystal values
-    function reset() {
-        if (wins++)
-            usertotal= 0;
-        wins= 0;
-        losses= 0;
-
-        if (losses++)
-            usertotal= 0;
-        wins= 0;
-        losses= 0;
-
+        //logging values into the html elements
+        //working thus far
+        $("#numberToGuess").text(targetnumber);
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+        //this part is working as expected
+        function init(){
+              $(".crystals").each(function(images)
+              {
+                for (i = 0; 1 < 4; i++)
+                {
+              var randomnum = Math.floor(Math.random() * 12) + 1; 
+            }
+          console.log(randomnum);
+        })
+            };
+    
+        function onclick () {
+           $(".crystals").each(images).onclick("click", function(){
+            var crystalval = ($(this).randomnum);
+    
+            usertotal += crystalval;
+            $("#YourTotal").text(usertotal);
+    
+            if (usertotal === targetnumber) {
+                wins++;
+            $("#wins").append(wins++);
+            }
+    
+            else if (usertotal >= targetnumber) {
+                losses++;
+            $("#losses").append(losses++);
+            }
+           })
+    
+            
+        }
+    
+          
+            //not working not assigning images crystal values
+        function reset() {
+            if (wins++)
+                usertotal= 0;
+            wins= 0;
+            losses= 0;
+    
+            if (losses++)
+                usertotal= 0;
+            wins= 0;
+            losses= 0;
+    
+        }
+        //calling the functions
+        init();
+        reset();
+    
     }
-    //calling the functions
-    init();
-    onclick();
-    reset();
-
-
+    })
