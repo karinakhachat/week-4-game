c3e29da87c164f1f95aeb17a880a511c2c19dd79
Karina Khachatryan <karinakhachat@Karinas-MacBook-Air.local>
2018-02-28 15:06:36 -0500

    new update

--- assets/javascript/Javascript.js
+++ assets/javascript/Javascript.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     var targetnumber = Math.floor(Math.random() * 120) - 1;
 
     //logging values into the html elements
-    //working thus far
+    //logs the the target number to the div 
     $("#numberToGuess").text(targetnumber);
     $("#wins").text(wins);
     $("#losses").text(losses);
@@ -18,20 +18,21 @@ $(document).ready(function() {
             }
         }
 
-        //the onclick function should assign a randomnum to each crystal each time a user clicks
+//the onclick function should assign a randomnum to each crystal each time a user clicks
         function onclick() {
             $(".crystals").each(function(index, image) {
                     var randomnum = Math.floor(Math.random() * 12) + 1;
                     $(image).val(randomnum);
                 }
             }
-
+//function addon is adding the new total which is usertotal + randomnum value and 
+//appending it into the your total div
             function addon() {
                 var usertotal = randomnum + $("#YourTotal").val();
                 $("#YourTotal").text(usertotal);
 
             }
-
+//function log wins is going to compare the user total and target number and log appropriately
             function logwins() {
                 if (usertotal === targetnumber) {
                     wins++;
@@ -41,8 +42,6 @@ $(document).ready(function() {
                     $("#losses").append(losses++);
                 }
             }
-
-
         }
 
         //not working not assigning images crystal values
@@ -56,11 +55,12 @@ $(document).ready(function() {
                 usertotal = 0;
             wins = 0;
             losses = 0;
-
+            clear()
+            init()
         }
         //calling the functions
-        init()
-        reset()
+        
+        
       
     }
     }
