678b2ea314282e16a9a6c88b519a349068b8d901
Karina Khachatryan <karinakhachat@Karinas-Air.fios-router.home>
2018-02-27 20:28:57 -0500

    update

--- assets/javascript/Javascript.js
+++ assets/javascript/Javascript.js
@@ -2,18 +2,20 @@
 $(document).ready(function ()
 //the largest parent function start 
 {
-    var randomnum = Math.floor(Math.random() * 102 + 19);
+    
     var usertotal = 0;
     var wins = 0;
     var losses = 0;
+    var numberOptions = [10,5,3,7];
+    var targetnumber = 53;
     //logging values into the html elements
     //working thus far
-    $("#numberToGuess").text(randomnum);
+    $("#numberToGuess").text(targetnumber);
     $("#wins").text(wins);
     $("#losses").text(losses);
     //this part is working as expected
     function init() {
-        var CrysVals = {};
+        
         imageCrystal1[1] = Math.floor(Math.random() * 12 + 1);
         imageCrystal2[2] = Math.floor(Math.random() * 12 + 1);
         imageCrystal3[3] = Math.floor(Math.random() * 12 + 1);
@@ -25,37 +27,35 @@ $(document).ready(function ()
         $("#image4").on("click", CrystalVals(4));
         //not working not assigning images crystal values
 
-        function NewTotal(crystalKey) {
+        function NewTotal() {
             usertotal = usertotal + CrysVals[NewTotal];
             console.log("new total" + usertotal);
             $("#YourTotal").text(usertotal);
 
-            if (usertotal === randomnum) 
-            {
+            if (usertotal === randomnum) {
                 wins++;
                 $("#wins").text(wins);
             }
 
-            else if (usertotal > randomnum)
-            {
+            else if (usertotal > randomnum) {
                 losses++;
-            $("#losses").text(losses);
+                $("#losses").text(losses);
             }
+        }
     }
     function reset() {
         if (wins++)
-            usertotal.clear();
-            wins.clear();
-            losses.clear();
+            usertotal= 0;
+        wins= 0;
+        losses= 0;
 
         if (losses++)
-
-            usertotal.clear();
-            wins.clear();
-            losses.clear();
+            usertotal= 0;
+        wins= 0;
+        losses= 0;
 
     }
-
+    //calling the functions
     init();
     reset();
 
