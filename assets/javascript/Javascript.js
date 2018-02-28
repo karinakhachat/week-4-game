3871e2b5506c94353872bac7d31c59ea9482608b
Karina Khachatryan <karinakhachat@Karinas-MacBook-Air.local>
2018-02-28 15:01:26 -0500

    line 49 broken

--- assets/javascript/Javascript.js
+++ assets/javascript/Javascript.js
@@ -1,60 +1,67 @@
-$(document).ready(function (){
+$(document).ready(function() {
     //the largest parent function start 
-        var usertotal = 0;
-        var wins = 0;
-        var losses = 0
-        var targetnumber = Math.floor(Math.random()* 120) -1;
-      
-        //logging values into the html elements
-        //working thus far
-        $("#numberToGuess").text(targetnumber);
-        $("#wins").text(wins);
-        $("#losses").text(losses);
-        //this part is working as expected
-        function init(){
-              $(".crystals").each(function(image){
-              var randomnum = Math.floor(Math.random() * 12) + 1; 
-                $(image).val(randomnum);}
-        }
-           
-    //the onclick function should assign a randomnum to each crystal each time a user clicks
-        function onclick () {
-            $(".crystals").each(function(index, image){
+    var usertotal = 0;
+    var wins = 0;
+    var losses = 0
+    var targetnumber = Math.floor(Math.random() * 120) - 1;
+
+    //logging values into the html elements
+    //working thus far
+    $("#numberToGuess").text(targetnumber);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    //this part is working as expected
+    function init(){
+        $(".crystals").each(function(image) {
                 var randomnum = Math.floor(Math.random() * 12) + 1;
-                  $(image).val(randomnum);}
-          }
-            function logwins (){
-            if (usertotal === targetnumber) {
-                wins++;
-            $("#wins").append(wins++);
+                $(image).val(randomnum);
             }
-    
-            else if (usertotal >= targetnumber) {
-                losses++;
-            $("#losses").append(losses++);
-            }
-           })
-    
-            
         }
-    
-            //not working not assigning images crystal values
-        function reset() {
+
+        //the onclick function should assign a randomnum to each crystal each time a user clicks
+        function onclick() {
+            $(".crystals").each(function(index, image) {
+                    var randomnum = Math.floor(Math.random() * 12) + 1;
+                    $(image).val(randomnum);
+                }
+            }
+
+            function addon() {
+                var usertotal = randomnum + $("#YourTotal").val();
+                $("#YourTotal").text(usertotal);
+
+            }
+
+            function logwins() {
+                if (usertotal === targetnumber) {
+                    wins++;
+                    $("#wins").append(wins++);
+                } else if (usertotal >= targetnumber) {
+                    losses++;
+                    $("#losses").append(losses++);
+                }
+            }
+
+
+        }
+
+        //not working not assigning images crystal values
+            function clear() {
             if (wins++)
-                usertotal= 0;
-            wins= 0;
-            losses= 0;
-    
+                usertotal = 0;
+            wins = 0;
+            losses = 0;
+
             if (losses++)
-                usertotal= 0;
-            wins= 0;
-            losses= 0;
-    
+                usertotal = 0;
+            wins = 0;
+            losses = 0;
+
         }
         //calling the functions
-        init();
-        reset();      usertotal += crystalval;
-            $("#YourTotal").text(usertotal);}
+        init()
+        reset()
       
     }
-}
+    }
+
